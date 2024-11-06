b91316b13f6ef42eabe18fb2aba9b91db09c315f
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2024-11-06 10:07:48 +0700

    chi so can nang

--- src/LeapYearCalculator.java
+++ src/LeapYearCalculator.java
@@ -5,18 +5,23 @@ public class LeapYearCalculator {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a year: ");
         int year = sc.nextInt();
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    System.out.printf("%d is leap year", year);
-                } else {
-                    System.out.printf("%d is not leap year", year);
+        boolean isLeapYear = false;
+        boolean isDivisibleBy4 = year % 4 == 0;
+        boolean isDivisibleBy100 = year % 100 == 0;
+        boolean isDivisibleBy400 = year % 400 == 0;
+        if (isDivisibleBy4) {
+            if (isDivisibleBy100) {
+                if (isDivisibleBy400) {
+                    isLeapYear = true;
                 }
             } else {
-                System.out.printf("%d is leap year", year);
+                isLeapYear = true;
             }
-        } else {
-            System.out.printf("%d is not leap year", year);
+        }
+        if(isLeapYear){
+            System.out.printf("%d is a Leap Year", year);
+        }else {
+            System.out.printf("%d is not a Leap Year", year);
         }
     }
 }
